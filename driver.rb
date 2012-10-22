9a98fa953f149d7a2c09f0285731941ede32f564
BinitKumar <binit.kumar.hbs@gmail.com>
2012-10-22 22:05:32 +0530

    Added new test cases

--- driver.rb
+++ driver.rb
@@ -4,65 +4,113 @@ require 'vfs'
 
 class Driver
   def initialize
+
+    #loading configuration
     settings = YAML.load_file('settings.yml')
     @config = settings['s3']
-    @sandbox = $sandbox || './sandbox'.to_dir.destroy
 
-    descriptor = Filea.new("dir_discripter.conf",'r')
+    #Creating virtual directory sandbox
+    @sandbox = $sandbox || './sandbox'.to_dir.destroy
+    @sandbox.create
+
+    @descriptor_file = 'dir_descriptor.conf'
+    @descriptor_file = 'test_dir_descriptor.conf' if $mode == 'test'
+
+    #loading previously created file and directory
+    descriptor = File.new(@descriptor_file,'r')
     while(line = descriptor.gets)
+      # All the fies has prefix of file_ otherwise it's a directory
       if line[0..4] == 'file_'
         @sandbox[ line[5..-1].strip ].write 'x'
       else
         @sandbox[line.strip].create
       end
     end
+
+    #Establishing AWS connection
     AWS::S3::Base.establish_connection!(:access_key_id => ENV['access_key_id'], :secret_access_key => ENV['secret_access_key'])
   end
-  
-  def authenticate(username, password )
-    user_file = File.new("user_details.conf", "r")
 
+  #Mehtod for authentication of user using user name and password
+  #If the user is valid user then a directory with name is being created
+  def authenticate(username, password )
+
+    #loading user details file
+    user_file = File.new("user_details.conf", "r")
 	  user_file = File.new("test_user_details.conf", "r") if $mode == 'test'
 
     while (line = user_file.gets)
       user_info = line.split(',')
+
+      #If user info exists in the user details file then user is authenticated
 	    if user_info[0] == username && user_info[1] == password
 
+        #if user already have directory then there is no need to create a new directory
         if @sandbox[username].exist? == false
           update_descriptor("./sandbox/#{username}")
 	        @sandbox[username].create
         end
+
+        #Setting username for the future use
         @username = username
+
 	      yield true
-	      return
+        return
 	    end
     end
     yield false
   end
-  
+
+  #To place non-streamed file on AWS Server
   def put_file(path, tmp_file_path)
     path = parse_path(path)
-    AWS::S3::S3Object.store(path, open(tmp_file_path), ENV['bucket'])
+    if $mode != 'test'
+      AWS::S3::S3Object.store(path, open(tmp_file_path), ENV['bucket']) if $mode != 'test'
+    else
+      #For unit test case only
+      $virtual_aws['path'] = path
+      $virtual_aws['bucket'] = ENV['bucket']
+    end
+    #Updating sandbox for file
     @sandbox[path].write 'x'
+
+    #Updating directory descriptor for directory persistence
     update_descriptor('file_'+path)
     yield File.size(tmp_file_path)
   end
 
+  #To place streamed file on aws server
   def put_file_streamed(path,file_stream)
     path = parse_path(path)
-    File.open(path, "ab+") do |f|
+
+    #Temprory hosting the file on virtual space
+    File.open(path, 'ab+') do |f|
       f.write(file_stream.data)
     end
 
     file_size = File.open(path).size
 
-    AWS::S3::S3Object.store(path, open(path), ENV['bucket'])
+    #Posting the streamed file to aws
+    if $mode != 'test'
+      AWS::S3::S3Object.store(path, open(path), ENV['bucket'])
+    else
+      #For unit test case only
+      $virtual_aws['path'] = path
+      $virtual_aws['bucket'] = ENV['bucket']
+    end
+
+    #overwriting the file with zero byte file
     @sandbox[path].write 'x'
     update_descriptor('file_'+path)
     yield file_size
   end
   
   def bytes(path)
+    path = parse_path(path)
+    if AWS::S3::S3Object.exist?(path,ENV['bucket'])
+      yield AWS::S3::S3Object.find(path,ENV['bucket'])['content-length']
+      return
+    end
     yield nil
   end
   
@@ -74,16 +122,20 @@ class Driver
       yield true
     end
   end
-  
+
+  #listing of files and folders of a specific path
   def dir_contents(path)
     files = Array.new
     path = parse_path(path)
     @vfs = @sandbox[path]
+
+    #if the path doesn't exist then it will show blank else the contents
     if @vfs.exist? == false
       yield []
     else
       @vfs.entries.each do |d|
-        filese << EM::FTPD::DirectoryItem.new(:name => d.name,
+        #converting all files to event machine format to match em-ftpd required format
+        files << EM::FTPD::DirectoryItem.new(:name => d.name,
                               :time => Time.now,
                               :permissions => 777,
                               :owner => @username,
@@ -94,30 +146,39 @@ class Driver
         yield files
     end
   end
-  
+
+  #Deletion of a directory
   def delete_dir(path)
     path = parse_path(path)
     update_descriptor(path,'delete')
     @sandbox[path].entries.each do |file|
+
+      #Deleting all the files from aws server
       if AWS::S3::S3Object.find(file,ENV['bucket'])
         AWS::S3::S3Object.delete(file, ENV['bucket'])
       end
       update_descriptor(file,'delete')
     end
+
     @sandbox[path].destroy
     yield true
   end
-  
+
+  #Deletion of a file
   def delete_file(path)
     path = parse_path(path)
+    #Deletion of file from aws server
     if AWS::S3::S3Object.find(path,ENV['bucket'])
       AWS::S3::S3Object.delete(path, ENV['bucket'])
     end
     update_descriptor('file_' + path,'delete')
+
+    #Deletion from virtual directory
     @sandbox[path].destroy
     yield true
   end
-  
+
+  #Rename a file
   def rename(from_path, to_path)
     from_path = parse_path(from_path)
     to_path = parse_path(to_path)
@@ -131,20 +192,29 @@ class Driver
     @sandbox[to_path].create
     yield true
   end
-  
+
+  #Create a new directory
   def make_dir(path)
     path = parse_path(path)
     update_descriptor(path)
     @sandbox[path].create
     yield true
   end
-  
+
+  #Download file from server
   def get_file(path)
     path = parse_path(path)
-    file = AWS::S3::S3Object.find(path, ENV['bucket'])
+    if $mode != 'test'
+      file = AWS::S3::S3Object.find(path, ENV['bucket'])
+    else
+      file = File.new(path)
+    end
     yield file.value
   end
 
+
+  private
+  #internal method to convert windows path to system specific path
   def parse_path(path)
     path_suffix = path.split('C:/')[-1]
 
@@ -159,11 +229,13 @@ class Driver
     path
   end
 
+  #Internal method to update directory descriptor
   def update_descriptor(path,method='create',dest_path=nil)
-    descriptor = File.new("dir_discripter.conf",'a+')
+    descriptor = File.new(@descriptor_file,'a+')
 
     if method == 'create'
       descriptor.puts(path)
+      descriptor.close
     else
       file_list = Array.new
       if method == 'delete'
@@ -180,7 +252,7 @@ class Driver
           end
         end
         descriptor.close
-        updated_descriptor = File.new("dir_discripter.conf","w")
+        updated_descriptor = File.new(@descriptor_file,"w")
         file_list.each do |line|
           updated_descriptor.puts line
         end
--- spec/driver_spec.rb
+++ spec/driver_spec.rb
@@ -1,22 +1,111 @@
 load 'driver.rb'
+
 $mode = 'test'
 describe Driver do
-  before(:all) do
+  before(:each) do
+    $virtual_aws = Hash.new
+    if Dir['./sandbox/test'].length == 1
+      FileUtils.rm_rf('./sandbox/test')
+    end
+    File.new('test_dir_descriptor.conf','w').puts nil
     @driver = Driver.new
+
+    @sandbox = $sandbox || './sandbox'.to_dir.destroy
+    @sandbox.create
   end
-  it "should authorise valid username and password" do
-    response = nil;
-	@driver.authenticate('test','test') do |resp| 
-	  response = resp; 
-	end
-	response.should == true
+
+  class VirtualSocket
+    def data
+      return 'abcd'
+    end
+  end
+
+  def check_existance_in_descriptor(path)
+    descriptor = File.new('test_dir_descriptor.conf','r')
+    entry_exists = false
+    while(line = descriptor.gets)
+      entry_exists = true if line.to_s.chomp == path
+    end
+    entry_exists.should == true
+  end
+  it "should authorise valid username and password and create directory for that user" do
+    response = nil
+	  @driver.authenticate('test','test') do |resp|
+	    response = resp
+    end
+
+    #Varifying user authentication request
+	  response.should == true
+
+    #Verifying direcotry with username created or not
+    Dir[ './sandbox/test'].length.should == 1
+
+    #vefirying directory descriptor updated or not
+    check_existance_in_descriptor './sandbox/test'.to_s.chomp
+
   end
   
   it "should not authorise invalid username" do
-    response = nil;
-	@driver.authenticate('test','invalidpassword') do |resp| 
-	  response = resp; 
-	end
-	response.should == false
+    response = nil
+	  @driver.authenticate('test','invalidpassword') do |resp|
+	    response = resp
+    end
+
+    #verifying invalid user authentication request
+	  response.should == false
+
+    #Verifying direcotry with username created or not
+    Dir[ './sandbox/test'].length.should == 0
+
+    #vefirying directory descriptor updated or not
+    descriptor = File.new('test_dir_descriptor.conf','r')
+    entry_exists = false
+    while(line = descriptor.gets)
+      entry_exists = true if line.to_s.chomp == './sandbox/test'.to_s.chomp
+    end
+    entry_exists.should == false
   end
+
+  it "should put non-streamed files to the server" do
+    response = nil
+    @driver.put_file('testdata/dummy_file.txt','testdata/dummy_file.txt') do | resp |
+      response = resp
+    end
+    response.should == File.new('testdata/dummy_file.txt').size
+    $virtual_aws['path'].should == './sandbox/testdata/dummy_file.txt'
+    $virtual_aws['bucket'].should == ENV['bucket']
+
+    check_existance_in_descriptor 'file_./sandbox/testdata/dummy_file.txt'
+
+  end
+
+  it "should put streamed file on server" do
+    file_stream = VirtualSocket.new
+    response = nil
+    @driver.put_file_streamed('streamed_file.txt',file_stream) do | resp |
+      response = resp
+    end
+
+    temp_file = File.new('testdata/test_dummy_file','w')
+    temp_file.write('abcd')
+
+    response.should == temp_file.size
+
+    $virtual_aws['path'].should == './sandbox/streamed_file.txt'
+    $virtual_aws['bucket'].should == ENV['bucket']
+
+    @sandbox['streamed_file.txt'].exist?.should == true
+    check_existance_in_descriptor('file_' + './sandbox/streamed_file.txt')
+
+    FileUtils.rm_rf('./sandbox/streamed_file.txt')
+  end
+
+  it "shoud provide the file byte size" do
+    @driver.bytes('streamed_file.txt') do | resp |
+      response = resp
+    end
+    resp.should == nil
+  end
+
+
 end
